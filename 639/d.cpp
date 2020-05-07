274c899d3292f7d471808956e67de815281a78aa
ekarpp <eetu.karppinen@aalto.fi>
2020-05-07 12:06:08 +0300

    Jfksdlf

--- 639/d.cpp
+++ 639/d.cpp
@@ -6,15 +6,40 @@
 typedef long long ll;
 
 void solve() {
-    int n;
+    int n, m;
     std::cin >> n;
-    std::vector<ll> vec(n);
-
+    std::cin >> m;
+    std::vector<bool> vec(n*m);
+    char c;
     for (int i = 0; i < n; i++)
-        std::cin >> vec[i];
+    {
+        for (int j = 0; j < m; j++)
+        {
+            std::cin >> c;
+            vec[j + i * n] = c == '.';
+        }
+    }
 
-    for (int i = 0; i < n; i++)
-        std::cout << vec[i];
+    bool found = false;
+    for (int col = 0; col < m; col++)
+    {
+        found = vec[col + 0 * n];
+        int row = 0;
+        while (vec[col + row*n])
+            row++;
+        row++;
+        for ( ; row < n; row++)
+        {
+            if (!vec[col + row*n])
+                continue;
+            if (!found)
+            {
+                found = true;
+                continue;
+            }
+            if (!vec[col + row*n])
+        }
+    }
 }
 
 
@@ -22,8 +47,8 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
 
-    int t;
-    std::cin >> t;
+    int t = 1;
+//    std::cin >> t;
     while (t--)
         solve();
 
